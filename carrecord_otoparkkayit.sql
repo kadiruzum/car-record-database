392d143aa196d67daf627594b6c2f84319c4f3b2
Kadir Üzüm <97547229+kadiruzum@users.noreply.github.com>
2024-01-19 21:37:23 +0300

    Update carrecord_otoparkkayit.sql

--- carrecord_otoparkkayit.sql
+++ carrecord_otoparkkayit.sql
@@ -1,56 +1,21 @@
--- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
---
--- Host: 127.0.0.1    Database: carrecord
--- ------------------------------------------------------
--- Server version	8.0.35
+-- Otopark kayıtları için tablo oluşturuyoruz
+-- Gereksinimlere gore ileride ucretlendirme sütunları koyulabilir
+CREATE TABLE IF NOT EXISTS OtoparkKayit (
+    KayitID INT PRIMARY KEY AUTO_INCREMENT,
+    Plaka VARCHAR(15) NOT NULL,
+    SahipAdi VARCHAR(50) NOT NULL,
+    IletisimBilgisi VARCHAR(20),
+    GirisTarihi DATETIME NOT NULL,
+    CikisTarihi DATETIME,
+    Durum VARCHAR(15) DEFAULT 'Otoparkta'
+);
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+-- Örnek kayıt
+INSERT INTO OtoparkKayit (Plaka, SahipAdi, IletisimBilgisi, GirisTarihi)
+VALUES ('ABC123', 'Ahmet Yılmaz', '555-1234', '2024-01-19 09:00:00');
 
---
--- Table structure for table `otoparkkayit`
---
+-- Arac çıkışı için örnek bir update işlemi
+UPDATE OtoparkKayit SET CikisTarihi = '2024-01-19 14:30:00', Durum = 'Çıkış Yapıldı' WHERE KayitID = 1;
 
-DROP TABLE IF EXISTS `otoparkkayit`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `otoparkkayit` (
-  `KayitID` int NOT NULL AUTO_INCREMENT,
-  `Plaka` varchar(15) NOT NULL,
-  `SahipAdi` varchar(50) NOT NULL,
-  `IletisimBilgisi` varchar(20) DEFAULT NULL,
-  `GirisTarihi` datetime NOT NULL,
-  `CikisTarihi` datetime DEFAULT NULL,
-  `Durum` varchar(15) DEFAULT 'Otoparkta',
-  PRIMARY KEY (`KayitID`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `otoparkkayit`
---
-
-LOCK TABLES `otoparkkayit` WRITE;
-/*!40000 ALTER TABLE `otoparkkayit` DISABLE KEYS */;
-INSERT INTO `otoparkkayit` VALUES (1,'ABC123','Ahmet Yılmaz','555-1234','2024-01-19 09:00:00','2024-01-19 14:30:00','Çıkış Yapıldı'),(2,'ABC123','Ahmet Yılmaz','555-1234','2024-01-19 09:00:00',NULL,'Otoparkta');
-/*!40000 ALTER TABLE `otoparkkayit` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2024-01-19 21:34:54
+-- Anlık olarak otoparkta olan araçları sorgu işlemi
+SELECT * FROM OtoparkKayit WHERE Durum = 'Otoparkta';
